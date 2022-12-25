4dab87ca08b7a0e9d40056a74f7eb0c8f7ab4388
codeCampWithSifat <sayedhossainsifat300@.com>
2022-12-26 02:18:40 +0600

    dynamic style done

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -6,10 +6,10 @@ import phone from "../../assets/icons/phone.svg"
 
 const Info = () => {
   return (
-    <div className='grid grid-cols-1 lg:grid-cols-3 gap-4'>
-      <InfoCards img={clock} />
-      <InfoCards img={marker} />
-      <InfoCards img={phone} />
+    <div className='grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-4'>
+      <InfoCards bgClass="bg-gradient-to-r from-secondary to-primary" cartTitle="Opening Hours"  img={clock} />
+      <InfoCards bgClass="bg-accent" cartTitle="Our Location" img={marker} />
+      <InfoCards bgClass="bg-gradient-to-r from-secondary to-primary" cartTitle="Contact Us" img={phone} />
     </div>
   )
 }
--- src/Pages/Home/InfoCards.js
+++ src/Pages/Home/InfoCards.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-const InfoCards = ({ img }) => {
+const InfoCards = ({ img, cartTitle, bgClass }) => {
   return (
-    <div class="card lg:card-side  shadow-xl bg-accent text-white px-4">
-      <figure>
+    <div class={`card lg:card-side  shadow-xl ${bgClass} text-white`}>
+      <figure className="pl-5 ">
         <img src={img} alt="Album" />
       </figure>
-      <div class="card-body">
-        <h2 class="card-title">New album is released!</h2>
+      <div class="card-body ">
+        <h2 class="card-title">{cartTitle}</h2>
         <p>Click the button to listen on Spotiwhy app.</p>
       </div>
     </div>
